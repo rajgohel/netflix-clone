c0526a364b1ae191934484a0c6a2be0a65a0fc24
Raj Gohel <rajgohel0001@gmail.com>
2023-12-02 12:52:05 +0530

    movie list and card component with effects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
-import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import useMovieList from '@/hooks/useMovieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,10 +23,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
